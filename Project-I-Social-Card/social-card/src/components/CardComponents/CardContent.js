d6905f93c1927fd6de89be034ee953b8073f6526
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-04-22 14:24:03 -0700

    first changes

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Card.css';
+
+
+function CardContent () {
+    return (
+        <div>
+            <p class="cardContent"><strong>Get started with React</strong><br/>
+            React makes it painless to create interactive UIs. Design simple views for each state in your application.
+            </p>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+export default CardContent;
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Hello ()
+{
+    return <h1>Hello</h1>;
+}
+export default Footer; 
